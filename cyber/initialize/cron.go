75384bc1ccf0d696db81419d70d0adba100568a6
DrReMain <51071534@qq.com>
2024-06-01 10:35:59 +0800

    fix: cron

--- cyber/initialize/cron.go
+++ cyber/initialize/cron.go
@@ -17,7 +17,7 @@ func Cron() {
 
 		{ // 模板定时任务
 			_, err := cyber.Cron.AddTaskByFunc("background", "@every 1m", func() {
-				hlog.Warnf("每一分钟打印一次日志 %s", time.Now().Format("2006-01-02 15:04:05"))
+				hlog.Warnf("每一分钟打印一次日志 %s\n", time.Now().Format("2006-01-02 15:04:05"))
 			}, "模板定时任务", option...)
 			if err != nil {
 				log.Fatalf("[Cron]: 添加定时任务失败: '%s'\n", err)
